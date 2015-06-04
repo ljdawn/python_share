fd6d54747dee7d8eeb791bfc9c84bcc69120e2ab
ljdawn <l.j.dawn@163.com>
2015-06-04 18:00:59 +0800

    small fix

--- get_related_tables.py
+++ get_related_tables.py
@@ -1,14 +1,25 @@
 #! /usr/bin env python
 
+import sys
 import os
+
+"""
+this file help u to find the related (foreign table) to the specified table
+it's for django
+"""
+if len(sys.argv) != 3:
+    print "usage python <paths> <table_name>"
+    sys.exit()
+
+paths, tb_name = sys.argv[1], sys.argv[2]
 
 lst = []
 ## "." is the paths
-for dirpath, dirnames, filenames in os.walk("."):
+for dirpath, dirnames, filenames in os.walk(paths):
     if "models.py" in filenames:
         lst.append(dirpath + "/models.py")
 
-current = []
+total = []
 for fl in lst:
     with open(fl, "r") as file:
         class_name, foreign_table, line_no = "", "", 0
@@ -16,9 +27,9 @@ for fl in lst:
             line_no += 1
             if line.startswith("class "):
                 class_name = line[6:] ## class name
-            elif  class_name and "Region" in line and "Field" in line :
-                current.append([fl, class_name, line])
+            elif  class_name and tb_name in line and "Field" in line :
+                total.append([fl, class_name, line])
             else:
                 pass
-for cur in current:
-    print cur
+for tot in total:
+    print tot
