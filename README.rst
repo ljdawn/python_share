a9fd0c13ec75aa4eae31be7f68268504a7aac485
ljdawn <l.j.dawn@163.com>
2015-06-10 14:22:07 +0800

    small change

--- README.rst
+++ README.rst
@@ -1,3 +1,3 @@
-author = "__asa_"_
+author = "__asa__"
 
 this is just some code pieces 
--- user_agents.py
+++
@@ -1,6 +0,0 @@
-import re
-MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
-class MobileMiddleware(object):
-    def process_request(self,request):
-        if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
-             request.urlconf="yourproj.mobile_urls"
